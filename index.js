21ae6a315404ed7306a1b39b70a5b45a800581de
Jannis R <mail@jannisr.de>
2018-11-28 23:31:24 +0100

    package entry point

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict'
 
-// todo
+const createRenderTime = require('./time')
+const createRenderArrival = require('./arrival')
+const createRenderDeparture = require('./departure')
+
+const create = (cfg = {}) => {
+	const {h, timezone, locale} = cfg
+	if ('function' !== typeof h) throw new Error('h must be a function')
+	if ('string' !== typeof timezone) throw new Error('timezone must be a string')
+	if ('string' !== typeof locale) throw new Error('locale must be a string')
+
+	const render = {}
+	render.time = createRenderTime(render, cfg)
+	render.arrival = createRenderArrival(render, cfg)
+	render.departure = createRenderDeparture(render, cfg)
+	return render
+}
+
+module.exports = create
